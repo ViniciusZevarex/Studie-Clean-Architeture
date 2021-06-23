0dd34e778d67054843f748f479a17d9d1b1b6aa2
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2021-06-23 13:38:30 -0300

    Criação das camadas da arquitetura. Implementação das entidades do domínio e suas regras de negócio.

---
+++ CleanArch.Domain/Entities/Category.cs
@@ -0,0 +1,46 @@
+﻿using CleanArch.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed  class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+            Name = name;
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid name. Name is required");
+            ValidateDomain(name);
+            Id = id;
+            Name = name;
+        }
+
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+
+        public ICollection<Product> Products { get; set; }
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name)
+                , "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+
+    }
+}

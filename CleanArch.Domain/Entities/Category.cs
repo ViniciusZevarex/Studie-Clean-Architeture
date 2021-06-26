60618cf601cd90daf00d03842a508980d6d9ff70
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2021-06-26 11:02:23 -0300

    Implementação da camada de testes do domínio e implementação do EF na camada de Infra.Data"

--- CleanArch.Domain/Entities/Category.cs
+++ CleanArch.Domain/Entities/Category.cs
@@ -15,7 +15,7 @@ namespace CleanArch.Domain.Entities
 
         public Category(int id, string name)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid name. Name is required");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
             ValidateDomain(name);
             Id = id;
             Name = name;
@@ -36,7 +36,7 @@ namespace CleanArch.Domain.Entities
             DomainExceptionValidation.When(string.IsNullOrEmpty(name)
                 , "Invalid name. Name is required");
 
-            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+            DomainExceptionValidation.When(name.Length < 3, 
                 "Invalid name, too short, minimum 3 characters");
 
             Name = name;
---
+++ CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArch.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Product category);
+        Task<Category> Update(Product category);
+        Task<Category> Remove(Product category);
+    }
+}
